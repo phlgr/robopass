70d0374052fe4cf5ce728e7b94f575ba9aa3e94a
phlgr <philippm.gartz@gmail.com>
2020-08-24 15:51:52 +0200

    Implement readfile

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 const questions = [
   {
@@ -8,17 +9,22 @@ const questions = [
   },
   {
     type: "input",
-    name: "name",
+    name: "key",
     message: "What password are you looking for?",
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
+inquirer.prompt(questions).then(async (answers) => {
   if (answers.password === "123") {
-    console.log("The Master Password is correct");
-    console.log(`Your ${answers.name} password is XXX`);
+    console.log("Master Password is correct!");
+    try {
+      const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
+      const passwords = JSON.parse(passwordsJSON);
+      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
+    } catch (error) {
+      console.error("Something went wrong 😑");
+    }
   } else {
     console.log("Incorrect Master Password");
-    return;
   }
 });
